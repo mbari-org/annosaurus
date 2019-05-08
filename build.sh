abf0a3bf4a44a30b08f3fdae85a65464b0e0baa0
Brian Schlining <bschlining@gmail.com>
2019-05-08 16:49:54 -0700

    Building 0.2.0

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 echo "--- Building annosaurus (reminder: run docker login first!!)"
 
 BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-VCS_REF=`git rev-parse --short HEAD`
+VCS_REF=`git tag | tail -1`
 
 
 sbt pack && \
