532438bdd108ec685a7666f7245c4892a797d543
Brian Schlining <bschlining@gmail.com>
2024-01-10 09:41:56 -0800

    #25 - Added SQL Server tests

--- it-sqlserver/src/test/scala/org/mbari/annosaurus/controllers/SqlServerObservationControllerSuite.scala
+++ it-sqlserver/src/test/scala/org/mbari/annosaurus/controllers/SqlServerObservationControllerSuite.scala
@@ -15,9 +15,10 @@
  */
 
 package org.mbari.annosaurus.controllers
-import org.mbari.annosaurus.repository.jpa.{PostgresTestDAOFactory, TestDAOFactory}
+import org.mbari.annosaurus.repository.jpa.{SqlServerTestDAOFactory, TestDAOFactory}
 
-class PostgresFastAncillaryDataControllerSuite extends FastAncillaryDataControllerITSuite {
-    override given daoFactory: TestDAOFactory = PostgresTestDAOFactory
+class SqlServerObservationControllerSuite extends ObservationControllerITSuite {
+
+    override given daoFactory: TestDAOFactory = SqlServerTestDAOFactory
 
 }
